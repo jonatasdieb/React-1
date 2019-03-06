d1e7e83a9d4ffe3bdbe90bfbe3815c2aed4de501
Jonatas <jonatasdieb@gmail.com>
2019-03-06 00:24:39 -0300

    corrigindo submits

--- src/Components/Departamento/NovoDepartamento.js
+++ src/Components/Departamento/NovoDepartamento.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import api from '../../Services/DepartamentoService';
+import {logout} from '../../Services/AuthService';
 
 
 class NovoDepartamento extends Component {
 
-    novoDepartamento = () => {
+    novoDepartamento = (e) => {
+        e.preventDefault();
+
         const departamento = {
             Nome: this.refs.nome.value
         }
@@ -14,7 +17,7 @@ class NovoDepartamento extends Component {
                 this.props.getMessages(res.data, "ok"))
             .catch(error => {
                 if (error.response.status === 401) {
-                    window.location.replace("/")
+                    logout()
                 } else {
                     this.props.getMessages(error.response.data, "nok")
                 }
@@ -27,12 +30,12 @@ class NovoDepartamento extends Component {
     render() {
         return (
             <div className='float-right'>
-                <form className='form-inline'>
+                <form className='form-inline' onSubmit={this.novoDepartamento}>
                     <div className="form-group">
                         <label>Novo departamento: &nbsp;</label>
                         <input ref='nome' type="text" className="form-control" placeholder="Nome do departamento" />
                     </div>
-                    &nbsp; <button type="button" onClick={this.novoDepartamento} className="btn btn-succ">Salvar <i class="fas fa-check text-white"></i></button>
+                    &nbsp; <button type="submit" className="btn btn-succ">Salvar <i class="fas fa-check text-white"></i></button>
                 </form>
             </div>
 
